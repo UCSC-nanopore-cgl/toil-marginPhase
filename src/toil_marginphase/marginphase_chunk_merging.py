d3ac183fa72dcf9f136561c6e26a8a6b314db579
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-18 04:28:20 +0000

    Adding tarfile member sanitization to extractall()

--- src/toil_marginphase/marginphase_chunk_merging.py
+++ src/toil_marginphase/marginphase_chunk_merging.py
@@ -665,7 +665,29 @@ def merge_chunks__extract_chunk_tarball(job, config, tar_work_dir, chunk):
     # get file
     job.fileStore.readGlobalFile(chunk[CI_OUTPUT_FILE_ID], tar_file, mutable=True)
     with tarfile.open(tar_file, 'r') as tar:
-        tar.extractall(tar_work_dir)
+        
+        import os
+        
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, tar_work_dir)
 
     # find desired files
     sam_unified, vcf = None, None
